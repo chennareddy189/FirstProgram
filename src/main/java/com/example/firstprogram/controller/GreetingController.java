1ad7593fc614f53066d77645eefc6b5189dc76df
chenn <dadireddychennareddy3@gmail.com>
2025-11-02 01:38:39 +0530

    controller with hello world statement

---
+++ src/main/java/com/example/firstprogram/controller/GreetingController.java
@@ -0,0 +1,11 @@
+package com.example.firstprogram.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greet() {
+        return "Hello, World!";
+    }
+}
